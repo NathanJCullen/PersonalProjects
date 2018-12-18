806b32ebbe1fa2fc6d51ca8b4beff7ceff4027e9
NathanJCullen <45045903+NathanJCullen@users.noreply.github.com>
2018-12-18 22:50:55 +0000

    Add files via upload

---
+++ PopUpManager.py
@@ -0,0 +1,69 @@
+from pygame import mixer
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+import webbrowser
+import time
+import ntpath
+
+soundVar = ""
+fileVar = ""
+
+def getSoundName():
+	global soundVar
+	soundPath = askopenfilename()
+	soundVar = ntpath.basename(soundPath)
+
+def getFileName():
+	global fileVar
+	filePath = askopenfilename()
+	fileVar = ntpath.basename(filePath)
+
+def stop():
+	root.destroy()
+
+def popupaction():
+	
+	timeVar = (int(entrytime.get())*1000)
+	
+	mixer.init()
+	mixer.music.load(str(soundVar))
+	    
+	if loop:
+	    root.after(timeVar, popupaction)
+	    webbrowser.open(str(fileVar))
+	    mixer.music.play()
+	else:
+		print("Popup stopped")
+
+root = Tk()
+root.title("Pop Up Manager")
+root.geometry("400x130")
+loop = True
+
+soundlabel = Label(root, text="Please input the sound of your popup; Include format")
+soundlabel.grid(column=0, row=0, sticky=W)
+# entrysound = Entry(root)
+# entrysound.grid(column=1, row=0)
+
+timelabel = Label(root, text="Please input the time between popups")
+timelabel.grid(column=0, row=1, sticky=W)
+entrytime = Entry(root)
+entrytime.grid(column=1, row=1)
+
+openlabel = Label(root, text="Please input the file to open on your popup; Inlcude format")
+openlabel.grid(column=0, row=2, sticky=W)
+# entryopen = Entry(root)
+# entryopen.grid(column=1, row=2)
+
+createbutton = Button(root, text="Create", command=popupaction)
+createbutton.grid(column=0, row= 3, columnspan=2)
+
+soundbutton = Button(root, text="Browse", command=getSoundName)
+soundbutton.grid(column=1, row= 0, sticky=W)
+
+filebutton = Button(root, text="Browse", command=getFileName)
+filebutton.grid(column=1, row= 2, sticky=W)
+
+Button(root, text="Quit Popup", command=stop).grid(column=0, row=4, columnspan=2)
+
+root.mainloop()
